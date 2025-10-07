5754a8f7ab95d49b3f1f7ebda7f35fa1e457fddc
mileyvo <mtvo106@student.hbuhsd.edu>
2025-10-07 18:21:43 +0000

    10/07/2025

--- Main.java
+++ Main.java
@@ -6,7 +6,38 @@ public class Main
   {
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
+    // Problem 1
+    double target = 12.345;
+    System.out.println("Enter a number that is a double");
+    double input = scan.nextDouble();
 
+    if (input == target)
+    {
+      System.out.println("YES!");
+    }
+    else
+    {
+      System.out.println("NO");
+    }
+
+    // Problem 2
+    int target2 = 48;
+    System.out.println("Enter a number that is an int");
+    int input2 = scan.nextInt();
+
+    if (input2 == target2)
+    {
+      System.out.println("YES!!)");
+    }
+    else
+    {
+      System.out.println("NO!!");
+    }
+
+    // Problem 4
+    
+
+    
 
 
 
